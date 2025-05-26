86142da18e8b904a87e5c77f83d26077f7885fdf
Omwami-dev <barasaowabarasa@gmail.com>
2025-05-26 12:42:13 +0300

    made changes on finance_calculator.py file

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,14 @@
+income = input("Enter your monthly income")
+
+expenses = input("Enter your total monthly expenses") 
+ 
+income = 5000
+expenses = 4000
+savings = income - expenses 
+
+print("your monthly income is",(savings))
+projected_savings = savings * 12 +(savings * 12 * 0.05)
+
+print("your projected savings after one year is",(projected_savings))
+
+ 
