954dccf305b858a34224070ca0ba42a77bab8d5c
Omwami-dev <barasaowabarasa@gmail.com>
2025-05-30 20:11:34 +0300

    made some changes on  weather_advice.py

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather = sunny/rainy/cold
+print("What`s the weather like today?", (weather)):
+    if weather = sunny:
+        print("Wear a t-shirt and sunglasses. ")
+    if weather = rainy:
+        print("Don`t forget your umbrella and a raincoat. ")
+    if weather = cold:
+        print("Make sure to wear a warm clothe and a scarf. ")
+else weather = sunny/rainy/cold:
+    print("Sorry,I don`t have recommendations for this weather. ")
