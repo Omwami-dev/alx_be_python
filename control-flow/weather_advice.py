25f45122581e7b7f8ed6348848b1ec6e8b276f99
Omwami-dev <barasaowabarasa@gmail.com>
2025-05-30 22:15:33 +0300

    made some changes on weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,5 @@
 weather = "sunny" "rainy" "cold"
-print("What`s the weather like today? (sunny/rainy/cold): ") 
+print("What's the weather like today? (sunny/rainy/cold): ") 
 if weather > "sunny":
     print('Wear a t-shirt and sunglasses')
 elif weather > "rainy":
