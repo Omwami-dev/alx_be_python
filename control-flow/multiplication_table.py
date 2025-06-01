58bb45ed1b93b2b8adc95d247c5bb7252dd81135
Omwami-dev <barasaowabarasa@gmail.com>
2025-06-01 13:36:24 +0300

    made some changes on multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,9 +1,11 @@
-print("Multiplication table(1 to 10:\n)" )
+
 number = input("Enter a number to see its multiplication table: ")
 for i in range(1,11):
     for j in range(1,11):
-        print(f"{j} * {i} ={j * i}",end= "  ")
+        print(f"{j} * {i} = {j * i}",end="\t")
     print()
+    
+
 
     
           
