4624e4b02e2909b02d2b3cf0e323f213cde8eef0
Omwami-dev <barasaowabarasa@gmail.com>
2025-06-15 09:10:26 +0300

    made changes on bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self._account_balance = initial_balance
+    def deposit(self,amount):
+        if amount > 0:
+        self._account_balance += amount
+        else:
+        raise ValueError
+    def withdraw(self,amount):
+        #Deduct the specified amount from the account balance if funds are sufficient.
+        #Returns True if the transaction is successful, False otherwise.
+        self._account_balance = amount
+        if amount > self._account_balance:
+            return False
+        self._account_balance =- amount
+        return True
+    def display_balance():
+        #return curent balance in the account in a friendly manner.
+        return f"Current Balance: ${self._account_balance:.2f}"
+
