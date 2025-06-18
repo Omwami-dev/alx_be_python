fdbdc692c243800f2dc3d41eefe1efdd9b07a411
Omwami-dev <barasaowabarasa@gmail.com>
2025-06-18 10:27:36 +0300

    made changes on main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -2,10 +2,12 @@ import sys
 from robust_division_calculator import safe_divide
 def main():
     if len(sys.agrv) !=3:
-        print("usage:python main.py<numerator><denominator>") sys.exit(1)
+        print("usage: python main.py<numerator><denominator>") 
+        sys.exit(1)
         numerator = sys.argv[1]
         denominator = sys.argv[2]
         result = safe_divide(numerator,denominator)
         print(result)
-        if __name__ == "_main_"
+        if __name__ == "_main_":
+            (main)
 
